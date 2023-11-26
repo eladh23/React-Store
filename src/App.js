5228dc464e6a45ee0f087062c3bba7ce9844d658
eladh23 <eladelad121212@gmail.com>
2023-11-26 16:30:06 +0200

    updates the packages

--- src/App.js
+++ src/App.js
@@ -1,110 +1,130 @@
 import axios from "axios";
-import React, { useState, useEffect } from "react";
+import { useEffect, useState } from "react";
 import Product from "./components/Product";
 import Navbar from "./components/Navbar";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Login from "./components/Login";
 import NoPage from "./components/NoPage";
-import MyFooter from "./components/MyFooter";
-import Addproduct from "./components/Addproduct";
-import Home from "./components/Home";
+import Footer from "./components/MyFooter";
+import Register from "./components/Register";
 import Cart from "./components/Cart";
+import AddProduct from "./components/AddProduct";
+import { Alert } from "react-bootstrap";
 
 function App() {
-	const HOST_URL = "https://elad-django-back.onrender.com";
-	const [products, setProducts] = useState([]);
-	const [cateogries, setCategories] = useState([]);
-	const [filteredProducts, setFilteredProducts] = useState([""]);
+  // "https://elad-django-back.onrender.com/product?category="
+  const HOST_URL = "https://elad-django-back.onrender.com";
+  const [categories, setCategories] = useState([]);
+  const [currentCategory, setCurrentCategory] = useState(1);
+  const [products, setProducts] = useState([]);
+  const [message, setMessage] = useState("");
+  const [showAlert, setShowAlert] = useState(false);
 
-	useEffect(getCategories, []); // when loading the page for the first time - getCategories()
+  useEffect(getProducts, [currentCategory]); // when loading the page for the first time - getProducts()
+  useEffect(getCategories, []); // when loading the page for the first time - getCategories()
+  // when category is clicked
+  function productAdded() {
+    setCurrentCategory("asgasgasg");
+    setCurrentCategory("");
+    setMessage("Product Added Succesfuly");
+    setShowAlert(true);
+  }
 
-	useEffect(() => {
-		// Fetch products when the component mounts
-		axios
-			.get(HOST_URL + "/?category=" + filteredProducts)
-			.then((res) => {
-				setProducts(res.data); // Update the products state
-			})
-			.catch((error) => {
-				console.error("Error fetching products:", error);
-			});
-	}, [filteredProducts]);
+  function clickButton(id) {
+    console.log("click!", id);
+    setCurrentCategory(id);
+  }
+  function getCategories() {
+    axios
+      .get(HOST_URL + "/category")
+      .then((response) => {
+        console.log("categories are:", response.data);
+        setCategories(response.data);
+      })
+      .catch((error) => {
+        console.error("Error fetching data:", error);
+      });
+  }
 
-	function getCategories() {
-		axios
-			.get(HOST_URL + "/category/")
-			.then((response) => {
-				console.log("categories", response.data);
-				setCategories(response.data);
-			})
-			.catch((error) => {
-				console.error("Error fetching data:", error);
-			});
-	}
+  function getProducts(searchText = null) {
+    console.log("!!!!!!!!!!!!!!", searchText);
+    let url = HOST_URL + "/product?category=" + currentCategory;
+    if (searchText) {
+      url = HOST_URL + "/product?search=" + searchText;
+    }
+    axios
+      .get(url)
+      .then((response) => {
+        console.log(response.data);
+        setProducts(response.data);
+      })
+      .catch((error) => {
+        console.error("Error fetching data:", error);
+      });
+  }
 
-	function handleCategoryClick(categoryId) {
-		console.log(categoryId);
-		setFilteredProducts(categoryId);
-	}
+  // example of filter in client
+  // function searchProduct(searchText) {
 
-	function searchproduct(filterdProductname) {
-		console.log(filterdProductname);
+  //   const filteredProducts = products.filter((product) =>
+  //   product.name.toLowerCase().includes(searchText.toLowerCase())
+  // );
+  // setProducts(filteredProducts);
+  // }
 
-		axios
-			.get(HOST_URL + "/?search=" + filterdProductname)
-			.then((response) => {
-				console.log("product", response.data);
-				if (response.data.length === 0) {
-					// Alert when no products are found
-					alert("No products found for the search");
-					// Update the products state with an empty array to clear any previous results
-					setProducts([]);
-				} else {
-					// Update the products state with the search results
-					setProducts(response.data);
-				}
-			})
-			.catch((error) => {
-				console.error("Error fetching data:", error);
-				// Handle the error, for instance, displaying an error message
-			});
+  function searchProduct(searchText) {
+    console.log("searching for product", searchText);
+    getProducts(searchText);
+    setCurrentCategory("stamsadgfsadhgdshrfdrah"); // setting the category so that the last category will work if clicked again.
+  }
 
-		setFilteredProducts(""); // Clear the search input
-	}
+  return (
+    <>
+      <BrowserRouter>
+        {showAlert && (
+          <Alert
+            variant="success"
+            onClose={() => setShowAlert(false)}
+            dismissible
+          >
+            {message}
+          </Alert>
+        )}
 
-	return (
-		<>
-			<BrowserRouter>
-				<Navbar
-					categories={cateogries}
-					handleCategoryClick={handleCategoryClick}
-					searchproduct={searchproduct}
-				/>
-				<Routes>
-					<Route
-						path="/"
-						element={
-							<>
-								<div className="container">
-									<div className="row">
-										{products.map((product) => (
-											<Product key={product.id} product={product} />
-										))}
-									</div>
-								</div>
-							</>
-						}
-					/>
-					<Route path="/login" element={<Login />} />
-					<Route path="/home" element={<Home />} />
-					<Route path="/add_product" element={<Addproduct />} />
-					<Route path="/cart" element={<Cart />} />
-					<Route path="*" element={<NoPage />} />
-				</Routes>
-				<MyFooter />
-			</BrowserRouter>
-		</>
-	);
+        <Navbar
+          categories={categories}
+          clickButton={clickButton}
+          searchProduct={searchProduct}
+        />
+        <Routes>
+          <Route
+            path="/"
+            element={
+              <>
+                <div className="row row-cols-1 row-cols-md-3 row-cols-lg-6 g-4">
+                  {products.map((product) => (
+                    <div key={product.id} className="col">
+                      <Product product={product} />
+                    </div>
+                  ))}
+                </div>
+                <br />
+              </>
+            }
+          />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/cart" element={<Cart />} />
+          <Route
+            path="/add_product"
+            element={<AddProduct productAdded={productAdded} />}
+          />
+          <Route path="*" element={<NoPage />} />
+        </Routes>
+        <Footer />
+      </BrowserRouter>
+    </>
+  );
 }
 
 export default App;
--- src/components/MyFooter.js
+++ src/components/MyFooter.js
@@ -1,31 +1,47 @@
-import React from "react";
+import { Link } from "react-router-dom";
 
-function MyFooter() {
+function Footer() {
   return (
-    <footer style={footerStyle}>
-      <div style={containerStyle}>
-        <p>&copy; {new Date().getFullYear()} My Store. All Rights Reserved.</p>
-        <p>Designed with React</p>
+    <>
+      <div className="container">
+        <footer className="py-3 my-4">
+          <ul className="nav justify-content-center border-bottom pb-3 mb-3">
+            <li className="nav-item">
+              <Link to="/" className="nav-link px-2 text-muted">
+                Home
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link to="/login" className="nav-link px-2 text-muted">
+                Login
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link to="/register" className="nav-link px-2 text-muted">
+                Register
+              </Link>
+            </li>
+            <li className="nav-item">
+              <a
+                href="https://www.google.com"
+                className="nav-link px-2 text-muted"
+              >
+                Google
+              </a>
+            </li>
+            <li className="nav-item">
+              <a href="/" className="nav-link px-2 text-muted">
+                About
+              </a>
+            </li>
+          </ul>
+          <p className="text-center text-muted">© 2022 Company, Inc</p>
+          {/* local image in public folder example */}
+          <img src={process.env.PUBLIC_URL + "/logo192.png"} alt="logo" />
+        </footer>
       </div>
-    </footer>
+    </>
   );
 }
 
-// Styles
-const footerStyle = {
-  backgroundColor: "#333",
-  color: "white",
-  textAlign: "center",
-  padding: "20px 0",
-  position: "fixed",
-  left: "0",
-  bottom: "0",
-  width: "100%",
-};
-
-const containerStyle = {
-  maxWidth: "1200px",
-  margin: "0 auto",
-};
-
-export default MyFooter;
+export default Footer;
