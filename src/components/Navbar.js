d610e62fcf78c53abad1c83684f5bc5b445daed8
eladh23 <eladelad121212@gmail.com>
2023-11-27 22:07:03 +0200

    search update

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,14 +2,14 @@ import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import axios from 'axios';
 
-
-
 const Navbar = () => {
   const [categories, setCategories] = useState([]);
   const [searchQuery, setSearchQuery] = useState('');
+  const [products, setProducts] = useState([]);
+  const [filteredProducts, setFilteredProducts] = useState([]);
 
   useEffect(() => {
-    // Fetch categories from the backend
+    
     axios.get('https://elad-django-back.onrender.com/products/category')
       .then(response => {
         setCategories(response.data);
@@ -17,12 +17,24 @@ const Navbar = () => {
       .catch(error => {
         console.error('Error fetching categories:', error);
       });
+
+    // Fetch all products initially
+    axios.get('https://elad-django-back.onrender.com/products')
+      .then(response => {
+        setProducts(response.data);
+        setFilteredProducts(response.data); // Initialize filteredProducts with all products
+      })
+      .catch(error => {
+        console.error('Error fetching products:', error);
+      });
   }, []); // Empty dependency array to run the effect only once on mount
 
   const handleSearch = () => {
-    // Perform search action based on searchQuery
-    console.log('Search query:', searchQuery);
-    // Add logic here to handle the search functionality (e.g., make API request, filter data, etc.)
+    // Filter products based on the search query
+    const filtered = products.filter(product =>
+      product.name.toLowerCase().includes(searchQuery.toLowerCase())   
+    );
+    setFilteredProducts(filtered);
   };
 
   return (
@@ -38,7 +50,6 @@ const Navbar = () => {
           <li key={category}>
             <Link to={`/products?category=${category}`}>{category}</Link>
           </li>
-          
         ))}
         <li>
           <Link to="/Login">Login</Link>
@@ -52,8 +63,17 @@ const Navbar = () => {
           />
           <button onClick={handleSearch}>Search</button>
         </li>
-               {/* Add more navigation links as needed */}
       </ul>
+
+      {/* Display filtered products */}
+      <section>
+        {filteredProducts.map(product => (
+          <div key={product.id}>
+            <h3>{product.name}</h3>
+            {/* Display other product information */}
+          </div>
+        ))}
+      </section>
     </nav>
   );
 }
