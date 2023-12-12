f77b246b3e78cf1074b1a019b89a6d7cfb2e860f
eladh23 <eladelad121212@gmail.com>
2023-12-12 16:03:38 +0200

    add port fix

--- src/App.js
+++ src/App.js
@@ -20,7 +20,7 @@ const App = () => {
       <div className="App">
         <Navbar products={products} setProducts={setProducts} />
         <Routes>
-          <Route path="/React-Store" element={<Home />} />
+          <Route path="/home" element={<Home />} />
           <Route path="/products" element={<ProductsList products={products} setProducts={setProducts} />} />
           <Route path="/add-product" element={<Addpro />} />
           <Route path="/Login" element={<Login />} />
