1fad8df833264d546b69afdfdb5243bdd1764c24
eladh23 <eladelad121212@gmail.com>
2023-12-12 17:28:22 +0200

    footer update

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,7 +5,7 @@ const Footer = () => {
   return (
     <footer className="footer mt-5 py-4 bg-dark text-light">
       <div className="container text-center">
-        <p className="mb-0">&copy; 2023 Your Company Name. All rights reserved.</p>
+        <p className="mb-0">&copy; 2023 React-Store. All rights reserved.</p>
         <p className="mb-0">
       
           <Link to="/contact" className="text-light me-2">Contact</Link>
